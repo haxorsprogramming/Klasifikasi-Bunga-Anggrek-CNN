685b5784a4b0e80a04f9bbd6d8c6701bdf30c704
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2022-12-17 14:58:45 +0700

    update form training data

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import random
 UPLOAD_FOLDER = 'data_upload'
 BASE_URL = "http://127.0.0.1:5000/"
 
+anggrek_class = ["Dendrobium_Dindii", "Dendrobium_Startiotes", "Dendrobium_Taurinum"]
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -17,6 +19,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     dr = {'BASE_URL': BASE_URL}
     return render_template('home.html', dRes=dr)
+
+@app.route('/training-data')
+def trainingData():
+    dr = {'BASE_URL': BASE_URL}
+    return render_template('training-data.html', dRes=dr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
