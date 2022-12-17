249fe5e5d11f6a4c0dde009d748eab505a6d1834
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2022-12-17 14:46:48 +0700

    update

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
+from werkzeug.utils import secure_filename
+import pandas as pd
+import numpy as np
+import uuid
+import os
+import json
+import random
+
+UPLOAD_FOLDER = 'data_upload'
+BASE_URL = "http://127.0.0.1:5000/"
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    dr = {'BASE_URL': BASE_URL}
+    return render_template('home.html', dRes=dr)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
